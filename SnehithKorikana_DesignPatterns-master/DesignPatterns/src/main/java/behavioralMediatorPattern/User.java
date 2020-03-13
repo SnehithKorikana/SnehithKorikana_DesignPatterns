e6ee25c20464483ecaf64cfe1220c5412a088f7b
Snehith <ksnehith1999@gmail.com>
2020-03-13 21:50:41 +0530

    Design Patterns

---
+++ SnehithKorikana_DesignPatterns-master/DesignPatterns/src/main/java/behavioralMediatorPattern/User.java
@@ -0,0 +1,16 @@
+package behavioralMediatorPattern;
+public class User {
+	 private String name;
+	   public String getName() {
+	      return name;
+	   }
+	   public void setName(String name) {
+	      this.name = name;
+	   }
+	   public User(String name){
+	      this.name  = name;
+	   }
+	   public void sendMessage(String message){
+	      GameChat.showMessage(this,message);
+	   }  
+}
